bc4bcd73ff75f61e4dfdaeaddb005a7fd60778cc
nitramSB <martin.brunaes@gmail.com>
2022-02-02 08:02:12 +0100

    Add files via upload
    First commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * LaserCom.c
+ *
+ * Created: 02.02.2022 08:00:14
+ * Author : Martin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+		
+    }
+}
+
